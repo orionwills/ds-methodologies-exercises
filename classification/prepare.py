df98b1194c5b45cae34883b855feeeb56304a1ea
Orion Wills <orion.wills@gmail.com>
2019-04-01 09:48:58 -0500

    updated classification exercises

--- classification/prepare.py
+++ classification/prepare.py
@@ -8,40 +8,40 @@ from sklearn.preprocessing import MinMaxScaler
 
 def drop_i_columns(df):
     '''drops iris columns'''
-    return df.drop(columns=(['measurement_id', 'species_id']))
+    return df.drop(columns = (['measurement_id', 'species_id']))
 
 def rename_species(df):
     '''renames species_name to species'''
-    return df.rename(index=str, columns={'species_name': 'species'})
+    return df.rename(index = str, columns={'species_name': 'species'})
 
 def encode_species(df):
     '''encodes species'''
     encoder = LabelEncoder()
     encoder.fit(df.species)
-    return df.assign(species=encoder.transform(df.species))
+    return df.assign(species_encode = encoder.transform(df.species))
 
 def prep_iris(df):
     '''preps all of iris data'''
     return df.pipe(drop_i_columns)\
-        .pipe(drop_i_columns)\
+        .pipe(rename_species)\
         .pipe(encode_species)
 
 def handle_missing_values(df):
     '''sets missing values in embarked and embark_town'''
     return df.assign(
-        embark_town=df.embark_town.fillna('Other'),
-        embarked=df.embarked.fillna('O')
+        embark_town = df.embark_town.fillna('Other'),
+        embarked = df.embarked.fillna('O')
     )
 
 def drop_t_columns(df):
     '''drop deck'''
-    return df.drop(columns='deck')
+    return df.drop(columns = 'deck')
 
 def encode_embarked(df):
     '''encodes embarked'''
     encoder=LabelEncoder()
     encoder.fit(df.embarked)
-    return df.assign(embarked=encoder.transform(df.embarked))
+    return df.assign(embarked_encode = encoder.transform(df.embarked))
 
 def prep_titanic(df):
     '''preps all of titanic'''
